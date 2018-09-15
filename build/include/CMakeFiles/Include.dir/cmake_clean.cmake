9a317fd6c102cd85b86f3e8a7414f0b4467e39a0
FengodChen <a1176553293@outlook.com>
2018-09-15 11:01:33 +0800

    Work in VSCode

---
+++ build/include/CMakeFiles/Include.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Include.dir/pathGC.cc.o"
+  "CMakeFiles/Include.dir/pointGC.cc.o"
+  "libInclude.pdb"
+  "libInclude.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Include.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/include/CMakeFiles/Include.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Include.
+# This may be replaced when dependencies are built.
